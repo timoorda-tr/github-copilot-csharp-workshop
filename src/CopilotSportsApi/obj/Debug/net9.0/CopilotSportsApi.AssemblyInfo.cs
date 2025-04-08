27b6ab2e5f30f557c7b861c29bb5c5f45aa357b9
timoorda-tr <timoorda@etoro.com>
2025-04-08 22:59:28 +0300

    fix workshop

--- src/CopilotSportsApi/obj/Debug/net9.0/CopilotSportsApi.AssemblyInfo.cs
+++ src/CopilotSportsApi/obj/Debug/net9.0/CopilotSportsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CopilotSportsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ba0006ed23a5451dd29570b480a801813ddf9db")]
 [assembly: System.Reflection.AssemblyProductAttribute("CopilotSportsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CopilotSportsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
